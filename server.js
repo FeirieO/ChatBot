ea7075bac4fb330841d5e4759a8ce2f682d94ffb
feirie001@gmail.com <feirie001@gmail.com>
2021-07-21 01:18:41 +0100

    connect the message client

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const socketio = require('socket.io');
 
 
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -11,9 +12,26 @@ const io = socketio(server);
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Run when client connects
+io.on('connection', socket => {
+    console.log('Web Server Connection');
+
+    socket.emit('message', 'Welcome to ChatBot 👋!');
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    //Runs when clients disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    })
+
+});
+
 const PORT = 3400 || process.env.PORT;
 
 server.listen(3400, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
 
